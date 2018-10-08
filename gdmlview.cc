ba9b44d149ea298e269dad8a92eed62c1640c030
Wouter Deconinck <wdconinc@gmail.com>
2018-10-08 10:09:54 -0400

    Include boost program options for -o and -v flags

--- gdmlview.cc
+++ gdmlview.cc
@@ -11,16 +11,27 @@
 #include "G4UIQt.hh"
 #include "G4VisExecutive.hh"
 
+#include <boost/program_options.hpp>
+namespace po = boost::program_options;
+
+#include <string>
+using std::string;
+
 #include <sys/param.h>
 
 class DetectorConstruction: public G4VUserDetectorConstruction
 {
   public:
-    DetectorConstruction(const G4String& gdmlfile) { SetGDMLFile(gdmlfile); };
+    DetectorConstruction(const G4String& gdmlfile, bool validate, bool overlap) {
+      fGDMLValidate = validate;
+      fGDMLOverlapCheck = overlap;
+      SetGDMLFile(gdmlfile);
+    };
 
     G4VPhysicalVolume* Construct() {
-      G4bool fGDMLValidate = true;
-      G4bool fGDMLOverlapCheck = true;
+      G4cout << "Reading " << fGDMLFile << G4endl;
+      G4cout << "- schema validation " << (fGDMLValidate? "on": "off") << G4endl;
+      G4cout << "- overlap check " << (fGDMLOverlapCheck? "on": "off") << G4endl;
       // Change directory
       char cwd[MAXPATHLEN];
       if (!getcwd(cwd,MAXPATHLEN)) {
@@ -45,6 +56,8 @@ class DetectorConstruction: public G4VUserDetectorConstruction
     };
 
   private:
+    G4bool fGDMLValidate;
+    G4bool fGDMLOverlapCheck;
     G4String fGDMLPath;
     G4String fGDMLFile;
     void SetGDMLFile(G4String gdmlfile) {
@@ -70,27 +83,35 @@ class PrimaryGeneratorAction: public G4VUserPrimaryGeneratorAction
     virtual void GeneratePrimaries(G4Event*) { };
 };
 
-namespace gdmlview
-{
-  void PrintUsage() {
-    G4cerr << "Usage: " << G4endl;
-    G4cerr << " gdmlview [gdml file]" << G4endl;
-  }
-}
-
 int main(int argc, char** argv)
 {
+  bool validate = false;
+  bool overlap = false;
+  string gdmlfile;
+
+  po::options_description desc("Allowed options");
+  desc.add_options()
+    ("help,h", "Produce this help message")
+    ("gdmlfile,g", po::value<string>(&gdmlfile), "top level gdml file")
+    ("validate,v", po::bool_switch(&validate), "enable schema validation")
+    ("overlap,o",  po::bool_switch(&overlap),  "enable overlap check")
+  ;
+  po::positional_options_description p;
+  p.add("gdmlfile", -1);
+
   // Parse command line options
-  G4String gdmlfile;
-  if (argc > 0) gdmlfile = argv[1];
-  else {
-    gdmlview::PrintUsage();
+  po::variables_map povm;
+  po::store(po::command_line_parser(argc, argv).options(desc).positional(p).run(), povm);
+  po::notify(povm);
+
+  if (povm.count("h") || gdmlfile.size() == 0) {
+    G4cout << desc << G4endl;
     return 1;
   }
 
   // Run manager
   G4RunManager* rm = new G4RunManager;
-  rm->SetUserInitialization(new DetectorConstruction(gdmlfile));
+  rm->SetUserInitialization(new DetectorConstruction(gdmlfile,validate,overlap));
   rm->SetUserInitialization(new PhysicsList);
   rm->SetUserAction(new PrimaryGeneratorAction);
   rm->Initialize();
